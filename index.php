687a09af5cc2fd8f962563ea320603fa052d11ed
m3stiz0 <kmestizo@gmail.com>
2019-08-05 19:38:11 -0400

    Educa

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 	get_header();
-		/*require_once(Consentidog_TEMP_PARTS . '/home.php');
-		/*require_once(Consentidog_TEMP_PARTS . '/home_educa.php');
-		require_once(Consentidog_TEMP_PARTS . '/home_texto.php');
+		require_once(Consentidog_TEMP_PARTS . '/home.php');
+		require_once(Consentidog_TEMP_PARTS . '/home_educa.php');
+		/*require_once(Consentidog_TEMP_PARTS . '/home_texto.php');
 		require_once(Consentidog_TEMP_PARTS . '/home_servicios.php');
 		require_once(Consentidog_TEMP_PARTS . '/home_blog.php');*/
 	get_footer();
