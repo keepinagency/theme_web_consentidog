0ddf0ffe34512fceb8c5fdff5251f0b97a3eec85
m3stiz0 <kmestizo@gmail.com>
2019-08-08 16:49:03 -0400

    ConsentiNotas

--- temp_parts/home_blog.php
+++ temp_parts/home_blog.php
@@ -7,24 +7,23 @@ $nuevo_arreglo = new WP_Query(array(
 	'posts_per_page'=>3
 ));
 ?>
-<div>
-<div class="custom-tituloblog">ConsentiNotas</div>
+<div class="contenedor-blogHome">
     <?php if ($nuevo_arreglo->have_posts()) :?>
-        <div class="listas_blog">
-        
-        <?php
-        while ($nuevo_arreglo->have_posts()) :
-            $nuevo_arreglo->the_post();?>
-                <div class=link_blog>
-                    <a class="titulo_blog" href="<?php the_permalink(); ?>"><?php the_title(); ?>
-                        <div class="miniatura_blog"
-                            style="background-image: url('<?php echo the_post_thumbnail_url('');?>'); 
-                            height:400px;
-                            background-size: cover;"> &nbsp;</div>
-                    </a>
-                        <div class="extracto"><?php //the_excerpt();?></div>
-                </div>
-        <?php endwhile;?>
+        <div class="listas_blogHome">
+            <div class="custom-tituloblog">ConsentiNotas</div>
+            <?php
+            while ($nuevo_arreglo->have_posts()) :
+                $nuevo_arreglo->the_post();?>
+                    <div class=link_blog>
+                        <a class="titulo_blog" href="<?php the_permalink(); ?>"><?php the_title(); ?>
+                            <div class="miniatura_blog"
+                                style="background-image: url('<?php echo the_post_thumbnail_url('');?>'); 
+                                height:400px;
+                                background-size: cover;"> &nbsp;</div>
+                        </a>
+                            <div class="extracto"><?php //the_excerpt();?></div>
+                    </div>
+            <?php endwhile;?>
 		</div>
         
     <?php endif;?>
