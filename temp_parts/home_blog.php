88966edff9b86df80abea92beeeafe3db8ddd27f
m3stiz0 <kmestizo@gmail.com>
2019-08-08 18:50:32 -0400

    ConsentiNotas, aun faltan ajustes en servicios

--- temp_parts/home_blog.php
+++ temp_parts/home_blog.php
@@ -7,21 +7,19 @@ $nuevo_arreglo = new WP_Query(array(
 	'posts_per_page'=>3
 ));
 ?>
-<div class="contenedor-blogHome">
+<div class="row contenedor-blogHome">
     <?php if ($nuevo_arreglo->have_posts()) :?>
-        <div class="listas_blogHome">
+        <div class="listas_blogHome col-md-12">
             <div class="custom-tituloblog">ConsentiNotas</div>
             <?php
             while ($nuevo_arreglo->have_posts()) :
                 $nuevo_arreglo->the_post();?>
-                    <div class=link_blog>
-                        <a class="titulo_blog" href="<?php the_permalink(); ?>"><?php the_title(); ?>
-                            <div class="miniatura_blog"
+                    <div class="link_blog col-md-4">  
+                        <div class="miniatura_blog"
                                 style="background-image: url('<?php echo the_post_thumbnail_url('');?>'); 
                                 height:400px;
                                 background-size: cover;"> &nbsp;</div>
-                        </a>
-                            <div class="extracto"><?php //the_excerpt();?></div>
+                            <a class="titulo_blog" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                     </div>
             <?php endwhile;?>
 		</div>
