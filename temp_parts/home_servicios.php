eda48f668050fce7a6bcc1631cce9a22498e944f
Javier <javerne@gmail.com>
2019-08-13 15:55:23 -0400

    overlay con multiply en servicios y corrección de fuentes en style

--- temp_parts/home_servicios.php
+++ temp_parts/home_servicios.php
@@ -12,24 +12,38 @@ $nuevo_arreglo = new WP_Query(array(
     <?php if ($nuevo_arreglo->have_posts()) :?>
         <div class="listas_servicios">
             <?php
+            $i=0;
             while ($nuevo_arreglo->have_posts()) :
                 $nuevo_arreglo->the_post();?>
                     <a href="<?php the_permalink();?>">
                         <div class="link_servicios color22 found22">
-                            <div class="titulo_servicios text-uppercase text-left">
+                            <div id="titserv_<?= $i;?>" class="titulo_servicios text-uppercase text-left"
+                                onMouseOver="this.style.backgroundColor='#FE330A';
+                                             this.style.color='white';
+                                             document.getElementById('imgserv_<?= $i;?>').style.backgroundColor='#FE330A';
+                                             document.getElementById('imgserv_<?= $i;?>').style.backgroundBlendMode='multiply';"
+                                onMouseOut="this.style.backgroundColor='white';
+                                             this.style.color='#FE330A';
+                                             document.getElementById('imgserv_<?= $i;?>').style.backgroundColor=''"> 
                                 <?php the_title();?>
                             </div>
-                            <div class="miniatura_servicios"
+                            <div id="imgserv_<?= $i;?>" class="miniatura_servicios"
                                 style="background-image: url('<?php echo the_post_thumbnail_url('');?>'); 
                                         height:350px;
                                         background-size: cover;"
                                 onMouseOver="this.style.backgroundColor='#FE330A';
-                                             this.style.backgroundBlendMode='multiply'"
-                                onMouseOut="this.style.backgroundColor=''"> &nbsp;
+                                             this.style.backgroundBlendMode='multiply';
+                                             document.getElementById('titserv_<?= $i;?>').style.backgroundColor='#FE330A';
+                                             document.getElementById('titserv_<?= $i;?>').style.color='white';"
+                                onMouseOut="this.style.backgroundColor='';
+                                             document.getElementById('titserv_<?= $i;?>').style.backgroundColor='white';
+                                             document.getElementById('titserv_<?= $i;?>').style.color='#FE330A';"> &nbsp;
                             </div>
                         </div>
                     </a>  
-            <?php endwhile;?>
+                <?php
+                $i++; 
+            endwhile;?>
 		</div>
     <?php endif;?>
 </div>
