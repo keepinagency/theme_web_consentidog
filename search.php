3ca538e03d0edc669cadd2261ae9fe361aa2a586
m3stiz0 <kmestizo@gmail.com>
2019-08-27 13:37:12 -0400

    Busqueda

--- search.php
+++ search.php
@@ -1,24 +1,33 @@
 <?php
-/*
-Template Name: Search Page
-*/
-get_header(); ?>
-<div id="primary" class="content-area">
-<div id="content" class="site-content" role="main">
-<?php while ( have_posts() ) : the_post(); ?>
-<?php shape_content_nav( 'nav-above' ); ?>
-<?php get_template_part( 'content', 'single' ); ?>
-<?php shape_content_nav( 'nav-below' ); ?>
-<?php
-    // If comments are open or we have at least one comment, load up the comment template
-    if ( comments_open() || '0' != get_comments_number() ){
-        comments_template( '', true );
-    }
-    
-?>
-<?php endwhile; // end of the loop. ?>
-</div><!-- #content .site-content -->
-</div><!-- #primary .content-area -->
-
-
-<?php get_footer();?>
+/* Template Name: Busqueda */        
+get_header(); 
+?>   
+<div class="row contenedor-general col-md-12 pt-5 m-0">                
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post();?>
+        <div class="row p-0 m-0" style="background-image:url('<?php echo get_the_post_thumbnail_url(); ?>');
+                    background-size:100%; background-repeat:no-repeat; height: 600px;">
+            <div id="post-<?php //the_ID(); ?>" class="col-12 h-25 text-center m-0 p-0" >
+                <a class="titulo-blog w-100 h-100 d-flex align-items-center
+                            text-center" href="<?php the_permalink(); ?>" >      
+                    <h4 class="enlace-blog text-uppercase text-center text-white w-100">
+                        <?php the_title(); ?>
+                    </h4>
+                </a>
+            </div>
+            <div class="col-12 h-25 text-center m-0 p-0">
+                <?php the_excerpt(); ?>
+                <?php //echo "$s"; ?>
+            </div>
+            <div class="col-12 h-50 text-center m-0 p-0">
+                <?php //the_post_thumbnail('medium'); ?>
+                <?php //echo "$s"; ?>
+            </div>
+            <div class="row col-12 pt-5 m-0 h-75" >
+                <?php endwhile; else :?>
+                    <p>Disculpe no encontramos ningun reultado que coincidiera con su criterio de búsqueda.</p>                    
+                <?php endif; ?>
+            </div>
+        </div>
+</div>
+<?php get_footer(); ?>
